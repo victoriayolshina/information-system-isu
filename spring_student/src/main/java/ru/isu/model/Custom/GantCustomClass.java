ea73841b0387f412045226032bf602e24a0e0324
Mark Kislyannikov <kislyannikov17@gmail.com>
2021-06-06 02:09:56 +0800

    delete: comments

--- spring_student/src/main/java/ru/isu/model/Custom/GantCustomClass.java
+++ spring_student/src/main/java/ru/isu/model/Custom/GantCustomClass.java
@@ -3,14 +3,11 @@ package ru.isu.model.Custom;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class GantCustomClass {
-
-    //public String name;
 
     private String desc;
 
